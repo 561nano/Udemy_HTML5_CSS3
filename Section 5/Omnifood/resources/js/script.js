67ab2c6b4a9e5467de51a363073935938ea63a0b
Christian Serrano <nano561@icloud.com>
2019-07-30 19:23:27 -0400

    Updated Sticky Nav  in JS and Css
    Added: After image for sticky nav
    Updated: Stick nav in style.css, index.html & script.js
    Added: waypoint.min.js file

--- Section 5/Omnifood/resources/js/script.js
+++ Section 5/Omnifood/resources/js/script.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
-    /* This is only to test if jquery lib is working*/
-    $('h1').click(function () {
-        $(this).css('background-color', '#ff0000');
-    })
+
+    /* If scrolling down past section-features add sticky class else remove sticky class*/
+    /* TODO where does 'direction come from - jQuerry or Waypoint? */
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction === "down") {
+            $('nav').addClass('sticky')
+        } else {
+            $('nav').removeClass('sticky')
+        }
+    }, {
+        offset: '60px'
+    });
+
+
 });
+
+/* This is a demo of waypoint formatting
+var waypoints = $('#handler-first').waypoint(function(direction) {
+notify(this.element.id + ' hit 25% from top of window')
+}, {
+offset: '25%'
+*/
